2d0027055e5ff390bc138bb2e2bf7a04171abb18
Saule <saulegri@gmail.com>
2022-10-19 13:56:22 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import './App.css';
 
+function ChildrenWithTitle({children,title}){
+  return (
+    <div>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  )
+}
+
 function FirstReactComponent() {
   return (
     <div>
@@ -14,8 +23,11 @@ function App() {
       <h1>Hello world</h1>
       <p>You look beautiful</p>
       <FirstReactComponent />
+      <ChildrenWithTitle title="I am title">
+        I am children
+      </ChildrenWithTitle>
     </div>
   );
 }
 
-export default App;
+export default App
